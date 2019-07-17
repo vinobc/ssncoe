33d450e59ab99a9414b5fda7a5deb0364b0c18f1
root <root@dev-ssnerp.c.stable-splicer-183522.internal>
2019-07-17 03:33:23 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,15 @@
 from setuptools import setup, find_packages
 #from pip.req import parse_requirements
 import re, ast
-
 #get version from version variable in plaid_integration/init.py
 _version_re = re.compile(r'version\s+=\s+(.*)')
-
 with open('requirements.txt') as f:
     install_requires = f.read().strip().split('\n')
 
 with open('ssncoe/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1)))
 
-#requirements = parse_requirements("requirements.txt", session="")
+        #requirements = parse_requirements(“requirements.txt”, session="")
 
 setup(
                 name='ssncoe',
