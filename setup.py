97581a9100d1fc90d5fca7d6f81dab573529b920
root <root@dev-ssnerp.c.stable-splicer-183522.internal>
2019-07-17 03:21:57 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+#from pip.req import parse_requirements
 import re, ast
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
+#get version from version variable in plaid_integration/init.py
+_version_re = re.compile(r'version\s+=\s+(.*)')
 
-# get version from __version__ variable in ssncoe/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
 
 with open('ssncoe/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
+    version = str(ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1)))
+
+#requirements = parse_requirements("requirements.txt", session="")
 
 setup(
-	name='ssncoe',
-	version=version,
-	description='SSNCOE',
-	author='Dept. of IT, SSNCE',
-	author_email='vinobchanderr@ssn.edu.in',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
-)
+                name='ssncoe',
+                version=version,
+                description='ssncoe',
+                author='vinob',
+                author_email='vinobchanderr@ssn.edu.in',
+                packages=find_packages(),
+                zip_safe=False,
+                include_package_data=True,
+                install_requires=install_requires
+                #dependency_links=[str(ir._link) for ir in requirements if ir._link]
+                )
